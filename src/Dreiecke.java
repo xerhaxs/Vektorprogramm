c36cddb75bdf64e6b5b3890989f259797aba990b
xerhaxs <xerhaxs@protonmail.com>
2022-03-28 19:45:59 +0200

    Fix Dreieck Zeichen

--- src/Dreiecke.java
+++ src/Dreiecke.java
@@ -1,5 +1,5 @@
 public class Dreiecke extends Form {
-    public Dreiecke(double pHPosition, double pVPosition, int pGroesse, int pVGroesse) {
+    public Dreiecke(double pHPosition, double pVPosition, int pGroesse) {
         super(pHPosition,  pVPosition, pGroesse);
         this.zeichne();
     }
@@ -8,7 +8,7 @@ public class Dreiecke extends Form {
         hatStift.bewegeUm(zGroesse);
         hatStift.dreheUm(90);
         hatStift.bewegeUm(zGroesse);
-        hatStift.bewegeUm((Math.sqrt(Math.pow(zGroesse) + Math.pow(zGroesse))));
+        hatStift.bewegeUm((Math.sqrt(Math.pow(zGroesse, 2) + Math.pow(zGroesse, 2))));
     }
     public boolean istGetroffen(double pHPosition, double pVPosition) {
         return (Math.pow(pHPosition - hPosition(),2) + Math.pow(pVPosition - vPosition(),2) <= Math.pow(zGroesse,2));
